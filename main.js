8ecda10fbd909a92bd8160d12d86fe7bd677041f
Pedro Lucas <pp.pedro.sb@gmail.com>
2025-01-12 01:34:56 -0300

    quarto commit

--- main.js
+++ main.js
@@ -2,13 +2,17 @@ const nextIndex = (currentIndex, length) => (currentIndex + 1) % length;
 
 // Função para alternar os cards
 const autoToggleCards = () => {
-    // Recupera os elementos dos cards
     const cards = [...document.querySelectorAll('.card')]; // Converte NodeList para Array
 
-    // Identifica o card ativo no início
+    // A primeira verificação pode ser feita diretamente para garantir que um card inicial tenha a classe 'ativo'
     let currentIndex = cards.findIndex(card => card.classList.contains('ativo'));
+    
+    if (currentIndex === -1) {
+        // Se nenhum card estiver ativo, faz o primeiro card começar visível
+        currentIndex = 0;
+        cards[currentIndex].classList.add('ativo'); // Adiciona a classe 'ativo' ao primeiro card
+    }
 
-    // Configura o intervalo para alternar os cards automaticamente
     setInterval(() => {
         // Remove a classe "ativo" do card atual
         cards[currentIndex].classList.remove('ativo');
@@ -18,7 +22,7 @@ const autoToggleCards = () => {
 
         // Adiciona a classe "ativo" ao próximo card
         cards[currentIndex].classList.add('ativo');
-    }, 5000); // Troca de card a cada 3 segundos
+    }, 5000); // Troca de card a cada 5 segundos
 };
 
 // Executa a função ao carregar a página
