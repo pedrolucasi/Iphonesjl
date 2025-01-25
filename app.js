a97f3aeea98bed45bd6048864b187e23092009c7
mac <mac@MacBook-Air-de-mac.local>
2025-01-25 12:51:09 -0800

    att local storage

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const products = [
     { id: 5, name: "iPhone 12", price: 10999, image: "iphone12.webp" },
     { id: 6, name: "iPhone 12 Pro Max", price: 8499, image: "iphone12promax.webp" },
     { id: 7, name: "iPhone 15 Plus", price: 6799, image: "iphone15plus.png" },
-    { id: 8, name: "iPhone 14 Pro Max", price: 4499, image: "iphone14max.webp" }
+    { id: 8, name: "iPhone 14 Pro Max", price: 4499, image: "iphone14max.webp" },
+    { id: 9, name: "iPhone 15 pro", price: 5799, image: "iphone14max.webp"}
 ];
 
 // Função para renderizar os produtos no DOM
--- signup.js
+++ signup.js
@@ -6,3 +6,53 @@ document.getElementById('cardForm').addEventListener('submit', function (event) 
     alert(`Formulário enviado!\nNome: ${nome}\nE-mail: ${email}`);
     
   });
+
+  function salvarDados() {
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const telefone = document.getElementById('telefone').value;
+    const senha = document.getElementById('senha').value;
+  
+    localStorage.setItem('dados', JSON.stringify({ nome, email, telefone, senha }));
+    alert('Dados salvos com sucesso!');
+  }
+
+  const btnLogin = document.getElementById('btn-login');
+const modal = document.getElementById('modal-login');
+const span = document.getElementsByClassName("close")[0];
+const formLogin = document.getElementById('form-login');
+
+btnLogin.addEventListener('click', () => {
+  modal.style.display = "block";
+});
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+formLogin.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const senha = document.getElementById('senha').value;
+
+  // Recupera os dados do Local Storage
+  const dadosSalvos = localStorage.getItem('dados');
+  const usuario = JSON.parse(dadosSalvos);
+
+  // Verifica se os dados correspondem
+  if (usuario && email === usuario.email && senha === usuario.senha) {
+    alert('Login realizado com sucesso!');
+    // Redirecionar para a página restrita ou executar outra ação
+  } else {
+    alert('Email ou senha incorretos.');
+  }
+
+  modal.style.display = "none";
+});
